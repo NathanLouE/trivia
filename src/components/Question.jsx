73f37f5c02417ad076e0cf0308bae7cd7e4d5940
Student <student@Students-MacBook-Air.local>
2020-01-29 18:14:42 -0800

    question class

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
-// import components
+import formattedQuestion from "./questionClass.js";
 
 
 class Question extends Component {
-  
   render() {
     return (
       <div>
         Add your QuestionText, Reset Button and AnswerButtons here.
-          
       </div>
     );
   }
---
+++ src/components/QuestionClass.js
@@ -0,0 +1,23 @@
+import {question} from sample_data.json; 
+
+class Question {
+    constructor(questionObj) {
+        this.question = question_text;
+        this.answer1 = choices[0];
+        this.answer2 = choices[1];
+        this.answer3 = choices[2];
+        this.answer4 = choices[3];
+    }
+}
+
+function cleanDataSet() {
+    let cleanData = [];
+  for (let i = 0; i < question.length; i++) {
+      let q = new Question(question[i]);
+      cleanData.push(q);
+  }
+
+  return cleanData
+}
+const formattedData = cleanDataSet();
+export default formattedData;
