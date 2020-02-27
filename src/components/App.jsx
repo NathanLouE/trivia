32622cbcf17322376763ed6a47d81448106d18d3
Kenny <kennyzhang29@gmail.com>
2020-02-26 17:32:47 -0800

    origin master

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,40 @@
 import React, { Component } from 'react';
 import '../css/App.css';
 // import components
-import formattedData from './QuestionClass';
-import AnswerButton from './AnswerButton';
-import Question from './Question';
-import QuestionText from './QuestionText';
-import {buildFirebase} from '../clients/firebase.js';
-
-let database = buildFirebase();
-let databaseRef = database.ref("/questions");
-databaseRef.once("value").then(function(data) {
- const questions = data.val();
-// Do something with the questions
-});
-
-
+import Answers from './Answers.jsx';
+import Question from './Question.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {count: 0}
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState({count:this.state.count+1});
+  }
 
   render() {
     console.log(this.props.data);
+    const q = this.props.app[this.state.count];
     return (
       <div className="app">
         Trivia!
-        <QuestionText />
-        <AnswerButton />      
-
+        <button  className="switch" onClick={() => this.handleClick()}>Click</button>
+        <h1> <Question question={q.question_text}/> </h1>
+        <div className="btnContainer">
+          <div className="btnContainers">
+            <button className="ansButton"><Answer answer = {q.choices[0]}/></button>
+            <button className="ansButton"><Answer answer = {q.choices[1]}/></button>
+          </div>
+          <div className="btnContainers">
+            <button className="ansButton"><Answer answer = {q.choices[2]}/></button>
+            <button className="ansButton"><Answer answer = {q.choices[3]}/></button>
+          </div>
+        </div>
       </div>
-
     );
-    
   }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from 'react-dom';
 import './css/App.css';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {buildFirebase} from './clients/firebase.js'
+
+let questions = {}
+var db = buildFirebase();
+var dbRef = db.ref("/questions");
+dbRef.once("value").then(function(data) {
+    questions = Object.values(data.val());
+    console.log(questions);
+    ReactDOM.render(<App data={questions}/>, document.getElementById('root'));
+   });
