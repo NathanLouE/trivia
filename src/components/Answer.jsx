ce60f31e43568c5bdf09c3ef7ced6aaf6c9117fb
Kenny <kennyzhang29@gmail.com>
2020-02-26 18:05:29 -0800

    origin master

---
+++ src/components/Answer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Answer extends Component {
+
+    render () {
+        return (<div className="answer"> {this.props.answer} </div>);
+    }
+}
+export default Answer;
--- src/components/Answers.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Answers extends Component {
-
-    render () {
-        return (<div className="answers"> {this.props.answer} </div>);
-    }
-}
-export default Answers;
